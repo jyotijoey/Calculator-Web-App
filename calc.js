be852c82bff8b03b8cff9b77458211ad32d67539
jyotijoey <jburnwal5@gmail.com>
2021-05-15 23:03:36 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+const express= require("express");
+const bodyParser= require("body-parser");
+const app= express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req,res){
+    console.log(req.body.n2);
+    var num1= Number(req.body.n1);
+    var num2= Number(req.body.n2);
+    var result= num1+num2;
+    res.send("the sum is "+ result);
+});
+
+app.get("/contact", function(req,res){
+    res.send("email.com");
+});
+
+app.listen(4000, function(){
+    console.log("app started at 4000");
+});
